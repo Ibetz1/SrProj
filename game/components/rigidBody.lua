e3e9f7d32a01721cfbea6c097c6d6aa791a99b40
1SliiX1 <63432408+1SliiX1@users.noreply.github.com>
2022-04-11 15:31:42 -0700

    fixed clipping

--- game/components/rigidBody.lua
+++ game/components/rigidBody.lua
@@ -1,8 +1,8 @@
 local function aabb(x1, y1, w1, h1, x2, y2, w2, h2)
-    return x1 + w1 > x2 and 
-           x1 < x2 + w2 and
-           y1 + h1 > y2 and
-           y1 < y2 + h2
+    return x1 + w1 >= x2 and 
+           x1 <= x2 + w2 and
+           y1 + h1 >= y2 and
+           y1 <= y2 + h2
 end
 
 local comp = _Util.Object:new({
