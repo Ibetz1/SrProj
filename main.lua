1b366d39db65f96d28fe9b58dc3138c25b93c4c4
1SliiX1 <63432408+1SliiX1@users.noreply.github.com>
2022-04-13 00:56:08 -0700

    fully removed lighting engine

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 love.graphics.setDefaultFilter("nearest", "nearest")
 require("engine")
 local game = require("game")
-local stack, world, index = game(16, 16, 1, {0.12, 0.12, 0.12})
+local stack, world, index = game(16, 16, 1)
 
-local e = game.entities:block(100, 100, 32, 24, _Assets.machine, _Assets.machine_normal, _Assets.machine_glow)
-local g = game.entities:block(100, 100, 32, 24, _Assets.machine, _Assets.machine_normal, _Assets.machine_glow)
-local f = game.entities:block(100, 100, 32, 24, _Assets.machine, _Assets.machine_normal, _Assets.machine_glow)
-local z = game.entities:block(100, 100, 32, 24, _Assets.machine, _Assets.machine_normal, _Assets.machine_glow)
+local e = game.entities:block(100, 100, 32, 24, _Assets.machine)
+local g = game.entities:block(100, 100, 32, 24, _Assets.machine)
+local f = game.entities:block(100, 100, 32, 24, _Assets.machine)
+local z = game.entities:block(100, 100, 32, 24, _Assets.machine)
 
 world:addEntity(e)
 world:addEntity(g)
@@ -26,29 +26,8 @@ function love.update(dt)
 end
 
 function love.draw()
+
     stack:draw()
+
     -- love.graphics.print(love.timer.getFPS())
-end
-
-function love.keyreleased(k)
-    if k == "f11" then
-        local fullscreen = love.window.getFullscreen()
-
-        local w1, h1 = love.graphics.getWidth(), love.graphics.getHeight()
-
-        love.window.setFullscreen(not fullscreen)
-
-        local w2, h2 = love.graphics.getWidth(), love.graphics.getHeight()
-
-        local aspect = w2 / w1
-
-        if aspect > 1 then
-            world:setAspect(aspect)
-        else
-            world:setAspect(0)
-        end
-
-        world:setScale()
-        world:adjustScreenSize()
-    end
 end
